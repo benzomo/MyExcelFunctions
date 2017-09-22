4901a7279c07016a0c96ebe3e59c3c152b0d5231
Tom Mo <admin0@datavsnews.com>
2017-09-21 23:47:34 -0600

    Add project files.

---
+++ MyXL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyXL
+{
+    public class Class1
+    {
+    }
+}
